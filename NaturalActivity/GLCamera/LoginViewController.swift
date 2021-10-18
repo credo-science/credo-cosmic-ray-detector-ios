a3d5bd62d8a14ebb0fc47347cee7ad01daaa3343
Maciek <maciek.siadkowski@gmail.com>
2021-10-18 23:31:21 +0200

    Add swift Bridging Header

---
+++ NaturalActivity/GLCamera/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Cosmic Ray
+//
+//  Created by Maciek Siadkowski on 18/10/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+    }
+}
